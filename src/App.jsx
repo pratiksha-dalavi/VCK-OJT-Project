cc2376b3fee0071ce2e1937d512b7d23ba2e56b6
pratiksha-dalavi <pratikshadalavi46@gmail.com>
2025-06-19 20:03:10 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,45 @@ import AboutPage from "./pages2/AboutPage";
 import CoursesPage from "./pages2/CoursesPage";
 import AdmissionsPage from "./pages2/AdmissionsPage";
 import NotFoundPage from "./pages2/NotFoundPage";
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
+import { useState } from "react";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
 
 function App () {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
-    <Router>
-      <div className="main-layout">
-        <Header/>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/courses" element={<CoursesPage />} />
-            <Route path="/admissions" element={<AdmissionsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      <Footer/>
+     <>
+            <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Pratiksha Parashuram Dalavi"
+          studentPhotoUrl="/Images/pratiksha.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
       </div>
-    </Router>
+          <Router>
+            <div className="main-layout">
+              <Header/>
+              <div className="content">
+                <Routes>
+                  <Route path="/" element={<HomePage/>} />
+                  <Route path="/contact" element={<ContactPage />} />
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="/courses" element={<CoursesPage />} />
+                  <Route path="/admissions" element={<AdmissionsPage />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+              </div>
+              <ChatbotComponent/>
+            <Footer/>
+            </div>
+          </Router>
+        </>
   )
 }
 export default App;
